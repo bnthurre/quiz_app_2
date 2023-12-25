3dfe34ebe9cd4a77caf16271ce5a14a4180f249f
bnthurre <bushrahurre959@gmail.com>
2023-12-25 10:06:35 +0300

    using ternary expresion

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,17 +12,10 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-
-    super.initState();
-  }
-
+  var activeScreen = 'start-screen';
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -36,7 +29,56 @@ class _QuizState extends State<Quiz> {
                 Color.fromARGB(255, 207, 158, 8),
                 Color.fromARGB(255, 207, 157, 4)
               ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-              child: activeScreen)),
+              child: activeScreen == 'start-screen'
+                  ? StartScreen(switchScreen)
+                  : const QuestionsScreen())),
     );
   }
 }
+
+
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app_2/questions_screen.dart';
+// import 'package:quiz_app_2/start_screen.dart';
+
+// class Quiz extends StatefulWidget {
+//   const Quiz({super.key});
+
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+//   }
+// }
+
+// class _QuizState extends State<Quiz> {
+//   Widget? activeScreen;
+//   @override
+//   void initState() {
+//     activeScreen = StartScreen(switchScreen);
+
+//     super.initState();
+//   }
+
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = const QuestionsScreen();
+//     });
+//   }
+
+//   @override
+//   Widget build(context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           body: Container(
+//               decoration: const BoxDecoration(
+//                   gradient: LinearGradient(colors: [
+//                 Color.fromARGB(255, 207, 158, 8),
+//                 Color.fromARGB(255, 207, 157, 4)
+//               ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+//               child: activeScreen)),
+//     );
+//   }
+// }
