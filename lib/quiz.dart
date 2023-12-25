b312fd97f0aa2499b095fb197d9c77214b68bbca
bnthurre <bushrahurre959@gmail.com>
2023-12-25 10:14:19 +0300

    using if statement insteasd

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -21,6 +21,11 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
     return MaterialApp(
       home: Scaffold(
           body: Container(
@@ -29,16 +34,55 @@ class _QuizState extends State<Quiz> {
                 Color.fromARGB(255, 207, 158, 8),
                 Color.fromARGB(255, 207, 157, 4)
               ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-              child: activeScreen == 'start-screen'
-                  ? StartScreen(switchScreen)
-                  : const QuestionsScreen())),
+              child: screenWidget)),
     );
   }
 }
 
+////using ternary exp
+
+// import 'package:flutter/material.dart';
+// import 'package:quiz_app_2/questions_screen.dart';
+// import 'package:quiz_app_2/start_screen.dart';
+
+// class Quiz extends StatefulWidget {
+//   const Quiz({super.key});
+
+//   @override
+//   State<Quiz> createState() {
+//     return _QuizState();
+//   }
+// }
+
+// class _QuizState extends State<Quiz> {
+//   var activeScreen = 'start-screen';
+//   void switchScreen() {
+//     setState(() {
+//       activeScreen = 'questions-screen';
+//     });
+//   }
+
+//   @override
+//   Widget build(context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//           body: Container(
+//               decoration: const BoxDecoration(
+//                   gradient: LinearGradient(colors: [
+//                 Color.fromARGB(255, 207, 158, 8),
+//                 Color.fromARGB(255, 207, 157, 4)
+//               ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+//               child: activeScreen == 'start-screen'
+//                   ? StartScreen(switchScreen)
+//                   : const QuestionsScreen())),
+//     );
+//   }
+// }
 
 
 
+
+ ////using initstate method
 
 // import 'package:flutter/material.dart';
 // import 'package:quiz_app_2/questions_screen.dart';
